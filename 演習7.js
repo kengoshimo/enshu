e84582e6c7195e4832a873ea1b5dac237a0c0675
Kengo Shimotanaka <kengo.shimotanaka@haveatalk.jp>
2026-02-25 14:35:23 +0900

    Revert "演習7完全版"
    This reverts commit 94893772ab2582ebf8240b076bda60edfc19dced.

--- 演習7.js
+++ 演習7.js
@@ -1,14 +1,6 @@
 const time = document.getElementById("currentTime");    //timneに現在の日時を表示させるための要素を取得
-const updatetime = function(){
-    const now = new Date(); //現在の日時を取得するためのDateオブジェクトを作成
-    const display =new Intl.DateTimeFormat("ja-JP",{
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-    }).format(now).replaceAll("/","-");
+const updatetime = function(){　　　　　　　　　　　　　　//現在の日時を取得するための関数を定義
+    const now = new Date();                             //現在の日時を取得するためのDateオブジェクトを作成
     
     const year = now.getFullYear();
     const month = now.getMonth() + 1;  //月は0から始まるため、1を加える必要がある   
@@ -18,7 +10,7 @@ const updatetime = function(){
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
-    time.textContent = display;
+    time.textContent = `${year}-${month}-${day}  ${hours}:${minutes}:${seconds}`;  //変数の中の値をフォーマットして、time要素のテキストコンテンツに表示　$(変数)
 };
 
 
